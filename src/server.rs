0366272d92a227311b153d6da0fc9158e8cc8c98
funnymania <2amslekshun@gmail.com>
2021-11-03 02:05:11 +0000

    Receive secure cookies

--- src/server.rs
+++ src/server.rs
@@ -10,6 +10,7 @@ use uuid::Uuid;
 
 use std::fs;
 use std::io;
+use std::collections::HashMap;
 use std::io::Read;
 use std::io::Write;
 use std::net::{TcpListener, TcpStream};
@@ -45,9 +46,10 @@ impl Server {
         //It is not an Error for this buffer to be too small, so we won't catch it
         let mut req = [0; 2048];
 
-        //TODO: Implement the ability to limit use of this token by IP or Host.
+        //IMP: Implement the ability to limit use of this token by IP or Host.
         //API token only used for testing.
         let wuh_token = Uuid::parse_str("00ec183d-7446-4511-b624-4349e385d730").unwrap();
+        Server::add_token(&mut client, &wuh_token);
 
         // Split to different actions
         let mut response = String::new();
@@ -85,6 +87,8 @@ impl Server {
                     }
                 }
 
+                                        let string_req = Server::print_bytes(&req);
+                                        let cookie_map = Server::retrieve_cookies(&string_req);
                 let str_req = Server::whats_reqd(check_req);
                 println!("Path: {}", str_req.1);
                 match str_req.1.as_str() {
@@ -276,6 +280,46 @@ impl Server {
                         );
                         stream.write_all(response.as_bytes()).unwrap();
                     }
+                    "/edit-profile" => {
+                        let content = match Server::get_page("/views/edit-profile.html") {
+                                Ok(html) => html,
+                                Err(e) => format!("<html><body>Webpage was not formatted correctly, please @funnymania_ in case they are sleeping (Zzzz):<br><a href=\"https://twitter.com/funnymania_\">https://twitter.com/funnymania_</a><br><br>Error: {}</body></html>", e)
+                            };
+                        response = format!(
+                            "HTTP/1.1 200 OK\r\n\
+                                Content-Type: text/html\r\n\
+                                Content-Length: {}\r\n\r\n{}",
+                            content.len(),
+                            content
+                        );
+
+                        match stream.write_all(response.as_bytes()) {
+                            Err(msg) => {
+                                println!("Error: {}\n{}", msg, String::from_utf8_lossy(&req))
+                            }
+                            _ => (),
+                        }
+                    }
+                    "/user-profile" => {
+                        let content = match Server::get_page("/views/user-profile.html") {
+                                Ok(html) => html,
+                                Err(e) => format!("<html><body>Webpage was not formatted correctly, please @funnymania_ in case they are sleeping (Zzzz):<br><a href=\"https://twitter.com/funnymania_\">https://twitter.com/funnymania_</a><br><br>Error: {}</body></html>", e)
+                            };
+                        response = format!(
+                            "HTTP/1.1 200 OK\r\n\
+                                Content-Type: text/html\r\n\
+                                Content-Length: {}\r\n\r\n{}",
+                            content.len(),
+                            content
+                        );
+
+                        match stream.write_all(response.as_bytes()) {
+                            Err(msg) => {
+                                println!("Error: {}\n{}", msg, String::from_utf8_lossy(&req))
+                            }
+                            _ => (),
+                        }
+                    }
                     "/subscriber" => {
                         let mut content = (String::new(), String::new());
                         match Server::extract_body(&req) {
@@ -357,7 +401,8 @@ impl Server {
                                                     "GET" => {
                                                         let mut status_code = String::new();
 
-                                                        let token = "test";
+                                                        let token = "3950ce00-a853-4cbe-ba19-b44488ad4314";
+
                                                         let content = match Uuid::parse_str(
                                                             &api_call.value[1],
                                                         ) {
@@ -377,7 +422,7 @@ impl Server {
                                                                         match Server::get_identity(
                                                                             &mut client,
                                                                             user_uuid,
-                                                                            token,
+                                                                            &token,
                                                                             &wuh_token
                                                                         ) {
                                                                             Ok(user) => {
@@ -470,7 +515,11 @@ impl Server {
                                                     "PUT" => match Server::extract_body(&req) {
                                                         Ok(body) => {
                                                             let mut status_code = String::new();
-                                                            let token = "test";
+                                                            let mut token = match cookie_map.get("sess_token") {
+                                                                Some(token_v) => String::from(token_v),
+                                                                None => String::from(""),
+                                                            };
+
                                                             let content = match Server::increment_updates(
                                                                 &mut client,
                                                                 &wuh_token,
@@ -483,7 +532,7 @@ impl Server {
                                                                             &api_call.value[1],
                                                                         )
                                                                         .unwrap(),
-                                                                        token,
+                                                                        &token,
                                                                         &wuh_token
                                                                     ) {
                                                                         Ok(user) => {
@@ -552,11 +601,10 @@ impl Server {
                                                                         }
                                                                     };
 
-                                                                //TODO: what if user has cookies off?
                                                                 response = format!(
                                                                         "HTTP/1.1 {}\r\n\
-                                                                        Set-Cookie: sess_token={}; Expires=Tue, 19 Jan 2038; Secure; HttpOnly\r\n
-                                                                        Content-Type: text/html\r\n\
+                                                                        Set-Cookie: sess_token={}; Expires=Tue, 19 Jan 2038; Secure; HttpOnly\r\n\
+                                                                        Content-Type: application/json\r\n\
                                                                         Content-Length: {}\r\n\r\n{}",
                                                                         status_code,
                                                                         token,
@@ -777,6 +825,30 @@ impl Server {
         file
     }
 
+    pub fn retrieve_cookies(request: &str) -> HashMap<String, String> {
+       let mut cookie_map = HashMap::new();
+
+       //Break request into lines
+        let lines: Vec<&str> = request.lines().collect();
+
+        let mut cookie_word: Vec<&str> = Vec::new();
+        for line in lines {
+            let mut words: Vec<&str> = line.split(" ").collect();
+            if words.len() > 0 && words[0] == "Cookie:" {
+                cookie_word = words.split_off(1); 
+                break;
+            }
+        }
+
+        for cookie in cookie_word {
+            let stripped_cookie = cookie.replace(";", "");
+            let kv_cookie:Vec<&str> = cookie.split("=").collect();
+           cookie_map.insert(kv_cookie[0].to_string(), kv_cookie[1].to_string());
+        }
+
+       cookie_map
+    }
+
     /// Only GET, POST, and PUT valid atm
     pub fn whats_reqd(req: String) -> (String, String) {
         let mut lines: Vec<&str> = req.lines().collect();
@@ -942,32 +1014,49 @@ impl Server {
     pub fn increment_gets(client: &mut Arc<Mutex<postgres::Client>>, api_token: &Uuid) -> Result<json::JsonValue, json::JsonValue> {
         let mut client = client.lock().unwrap();
 
-        match client.query("UPDATE api_token SET get_user = get_user + 1 WHERE id = $1", &[api_token]) {
-            Ok(rows) => Ok(object! { count: rows[0].get::<&str, i32>("get_user") }),
-            Err(e) => Err(object! { error: String::from("Token does not exist")}),
+        match client.execute("UPDATE api_token SET get_user = get_user + 1 WHERE id = $1 RETURNING *", &[api_token]) {
+            Ok(res) => {
+                if res == 0 {
+                    Err(object! { error: String::from("Token does not exist")})
+                } else {
+                    Ok(object! { success: String::from("Record updated") })
+                }
+            }
+            Err(e) => {
+                Err(object! { error: String::from("Formatting error.")})
+            }
         }
     }
+    
     pub fn increment_registered_users(client: &mut Arc<Mutex<postgres::Client>>, api_token: &Uuid)  -> Result<json::JsonValue, json::JsonValue> {
         let mut client = client.lock().unwrap();
 
-        match client.query("UPDATE api_token SET reg_user = reg_user + 1 WHERE id = $1", &[api_token]) {
+        match client.execute("UPDATE api_token SET reg_user = reg_user + 1 WHERE id = $1", &[api_token]) {
             Ok(rows) => {
-                if rows.len() == 0 {
+                if rows == 0 {
                     Err(object! { error: String::from("API token not found.")})
                 } else {
-                    Ok(object! { count: rows[0].get::<&str, i32>("reg_user") })
+                    Ok(object! { success: String::from("Record updated") })
                 }
             }
             Err(e) => Err(object! { error: String::from("Some formatting issue on updating API token")}),
 
         }
     }
+
     pub fn increment_updates(client: &mut Arc<Mutex<postgres::Client>>, api_token: &Uuid)  -> Result<json::JsonValue, json::JsonValue> {
         let mut client = client.lock().unwrap();
 
-        match client.query("UPDATE api_token SET update_user = update_user + 1 WHERE id = $1", &[api_token]) {
-            Ok(rows) => Ok(object! { count: rows[0].get::<&str, i32>("update_user") }),
-            Err(e) => Err(object! { error: String::from("Token does not exist")}),
+        match client.execute("UPDATE api_token SET update_user = update_user + 1 WHERE id = $1", &[api_token]) {
+            Ok(rows) => {
+                if rows == 0 {
+                    Err(object! { error: String::from("API token not found.")})
+                } else {
+                    Ok(object! { success: String::from("Record updated") })
+                }
+            }
+            Err(e) => Err(object! { error: String::from("Some formatting issue on updating API token")}),
+
         }
     }
 
@@ -981,6 +1070,7 @@ impl Server {
     ) -> Result<json::JsonValue, json::JsonValue> {
         let mut client = client.lock().unwrap();
         let plain_sess_token = Server::decrypt(sess_token);
+        //FIXME: This might not be valid.
         let uuid_token = Uuid::parse_str(plain_sess_token.as_str()).unwrap();
         let res = client.query(
             "SELECT * FROM user_device WHERE sess_token = $1",
@@ -1018,7 +1108,8 @@ impl Server {
                     Ok(object! {
                         identity: {
                             id: rows[0].get::<&str, Uuid>("id").to_simple().to_string(),
-                            name: rows[0].get::<&str, String>("name")
+                            name: rows[0].get::<&str, String>("name"),
+                            pfp: rows[0].get::<&str, String>("pfp")
                         }
                     })
                 }
@@ -1027,7 +1118,7 @@ impl Server {
         }
     }
 
-    //TODO: user_identity table (id_id, user_id, name, configs, attributes) is the table. get_user
+    //IMP: user_identity table (id_id, user_id, name, configs, attributes) is the table. get_user
     //would mostly only be used for our own information. However there is another table,
     //user_general (this contains information that all identities might use, such as mailing
     //address, birth name, etc) and another table user_private (information that you do not want
@@ -1108,7 +1199,8 @@ impl Server {
                         Ok(object! {
                             identity: {
                                 id: rows[0].get::<&str, Uuid>("id").to_simple().to_string(),
-                                name: rows[0].get::<&str, String>("name")
+                                name: rows[0].get::<&str, String>("name"),
+                                pfp: rows[0].get::<&str, String>("pfp"),
                             }
                         })
                     }
@@ -1319,11 +1411,15 @@ impl Server {
         }
     }
 
-    pub fn print_bytes(req: &[u8]) {
-        println!("{}", String::from_utf8_lossy(req).trim_matches(char::from(0)));
+    pub fn print_bytes(req: &[u8]) -> String {
+        let req_str = String::from_utf8_lossy(req);
+        let trimmed = req_str.trim_matches(char::from(0));
+        println!("{}", trimmed);
+
+        String::from(trimmed)
     }
 
-    //TODO: Multi-device login
+    //IMP: Multi-device login
     pub fn authenticate_user(
         client: &mut Arc<Mutex<postgres::Client>>,
         body: &str,
@@ -1378,14 +1474,14 @@ impl Server {
                         match other_logins {
                             Ok(token_rows) => {
                                 let new_id = Uuid::new_v4();
-                                //TODO: Default ID 0 = (unknown), 1 = iphone, 2 = android, 3 = mac,
+                                //IMP: Default ID 0 = (unknown), 1 = iphone, 2 = android, 3 = mac,
                                 //4 = linux, 5 = windows
                                 let dev_type = "Unknown";
                                 let new_sess_token = Uuid::new_v4();
                                 let insert_res = client.query("INSERT INTO user_device (uid, id, device_type, sess_token, sess_date) VALUES ($1, $2, $3, $4, 'now')", &[&uid, &new_id, &dev_type, &new_sess_token]);
                                 match insert_res {
                                     Ok(_) => {
-                                        //TODO: Handle multiple identities (right now just
+                                        //IMP: Handle multiple identities (right now just
                                         //returning first).
                                         match client
                                             .query("SELECT * FROM identity WHERE uid = $1", &[&uid])
@@ -1419,6 +1515,15 @@ impl Server {
             }
         } else {
             Err(object! {error: String::from("'Name' field must be a string")})
+        }
+    }
+
+    pub fn add_token(client: &mut Arc<Mutex<postgres::Client>>, token: &Uuid) {
+        let mut client = client.lock().unwrap();
+        let zero = 0i64;
+        match client.execute("INSERT INTO api_token (id, get_user, reg_user, update_user) VALUES ($1, $2, $3, $4)", &[token, &zero ,&zero ,&zero]) {
+            Err(e) => println!("Could not insert: {}", e),
+            _ => {}
         }
     }
 
